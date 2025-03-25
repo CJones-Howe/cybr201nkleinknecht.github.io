c84c63ae7883c01619e1b419f8b8fcb82b6e5e0d
nkleinknecht <nkleinknecht@albany.edu>
2025-03-24 20:06:20 -0400

    Week 9
    javascript work

---
+++ Javascript work (week9)/output.js
@@ -0,0 +1,15 @@
+function showInnerHTML(){
+	document.getElementById("output1").innerHTML = "Random cool text!";
+}
+
+function useDocumentWrite(){
+	document.write("Oh no! You clicked the button that deletes everything!");
+}
+
+function showAlert(){
+	alert("This is not an alert box!");
+}
+
+function showConsole(){
+	console.log("This is a super cool and secret console message");
+}
---
+++ Javascript work (week9)/variables.js
@@ -0,0 +1,8 @@
+// Using let
+let name = "Nick";
+let age = 22;
+let isStudent = true;
+let amIHungry = true;
+
+document.getElementById("demo1").innerHTML =
+	"Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent + "<br>Am I Hungry: " + amIHungry;
